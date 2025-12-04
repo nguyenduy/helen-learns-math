9a8530d49c1b0940c9d0a385b49291953cdcfc43
Duy Nguyen <nkduy2010@gmail.com>
2025-12-03 22:38:11 -0500

    feat: set base URL for Vite configuration based on package homepage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import pkg from './package.json';
+
+const base = pkg.homepage ? new URL(pkg.homepage).pathname : '/';
 
 export default defineConfig({
+  base,
   plugins: [react()],
   build: {
     outDir: 'dist',
